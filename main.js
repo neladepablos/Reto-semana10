6592a9e12e5f7dc6a9753c77ab7e2c43cda1a390
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2020-02-22 04:39:56 +0100

    ejercicios Javascript Ecmascript semana 10

--- main.js
+++ main.js
@@ -89,6 +89,20 @@
 
 // console.log(formatPhoneNumber(numeros));
 
+//7. Optimizado:
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// function phonenumber(numbers) {
+//     let three = numbers.slice(0,3).join('');
+//     let rest = numbers.slice(3,6).join('');
+//     let final = numbers.slice(6,10).join('');
+//     let phone = `(${three}) ${rest} - ${final}`;
+//     return phone; 
+// }
+
+// console.log(phonenumber(numbers));
+
 /*------------------------------------------------------------------------------------------------------------*/
 //8. Cree una función que tome una matriz de matrices con números.
 //Devuelve una nueva matriz (única) con el mayor número de cada uno.
@@ -125,11 +139,212 @@
 //     let primera = palabra.charAt(0);
 //     let ultima = palabra.charAt(palabra.length-1);
 
+
 //     return [primera, ultima];
 // }
 
 // console.log(primUlt(palabra));
 
+/*------------------------------------------------------------------------------------------------------------*/
+//10. Escriba una función que convierta un objeto en una matriz,
+// donde cada elemento representa un par clave-valor.
+//toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
 
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3    
+// };
+// function objectToMatriz(obj) {
+//     let arr = [];
+//     Object.keys(obj).forEach(function(key) {
+//         let valor = obj[key];
+//         let arr2 = [key, valor];
+//         arr.push(arr2);      
+//       });
+
+//       return arr;
+// }
+
+// console.log(objectToMatriz(obj));
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 11. Cree la función que toma una matriz con objetos y 
+//devuelve la suma de los presupuestos de las personas.
+
+// getBudgets([
+//     { name: "John", age: 21, budget: 23000 },
+//     { name: "Steve",  age: 32, budget: 40000 },
+//     { name: "Martin",  age: 16, budget: 2700 }
+//   ]) ➞ 65700
+
+// let arr = [
+//     { name: "John", age: 21, budget: 23000 },
+//     { name: "Steve",  age: 32, budget: 40000 },
+//     { name: "Martin",  age: 16, budget: 2700 },
+//     { name: "Mario",  age: 16, budget: 3000 }
+
+
+// ];
+
+// function sumaNumeros(arr) {
+   
+//     let suma = 0;
+//     for(i = 0; i < arr.length; i++) {
+//         suma = suma + arr[i].budget;
+//     }
+
+//     return suma;
+// }
+
+// console.log(sumaNumeros(arr));
+
+/*------------------------------------------------------------------------------------------------------------*/
+//12. Cree una función que tome una matriz de estudiantes y 
+//devuelva una matriz de nombres de estudiantes.
+
+// getStudentNames([
+//     { name: "Steve" },
+//     { name: "Mike" },
+//     { name: "John" }
+//   ]) ➞ ["Becky", "John", "Steve"]
+  
+// let students = [
+//     {name: "Oscar"},
+//     {name: "Marianela"},
+//     {name: "Luis"},
+//     {name: "pepe"}
+// ];
+
+
+// function NombreStudents(students) {
+//      let arr = [];
+//     for(i=0; i< students.length; i++) {     
+
+//        arr.push(students[i].name);
+//     }
+
+//     return arr;
+// }
+
+// console.log(NombreStudents(students));
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 13. Escriba una función que convierta un objeto en una matriz de claves y valores.
+
+// objectToArray({
+//     likes: 2,
+//     dislikes: 3,
+//     followers: 10
+//   }) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 14. Cree una función donde, dado el número n, devuelva la suma de todos los números cuadrados  incluyendo n.
+// squaresSum(3) ➞ 14
+// // 1² + 2² + 3² =
+// // 1 + 4 + 9 =
+// // 14
+
+  
+
+// function sumaCuadrados(n) {
+//             let suma = 0;
+//             for ( i = 1; i <= n; i++) {
+//                 suma = suma + Math.pow(i,2);
+//             }
+//             return suma;    
+//         }
+        
+// console.log(sumaCuadrados(3));
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 15. Cree una función para multiplicar todos los valores en una matriz por la cantidad
+// de valores en la matriz dada
+//multiplyByLength([2, 3, 1, 0]) ➞ [8, 12, 4, 0]
+
+// let arr = [2, 3, 1, 0];
+
+// let resul = arr.map(function(x) {
+//     return x * arr.length;
+// });
+
+// console.log(resul);
+
+/*------------------------------------------------------------------------------------------------------------*/
+//16 .Cree una función que tome un número como argumento y devuelva una matriz de 
+//números contando desde este número a cero.
+//countdown(5) ➞ [5, 4, 3, 2, 1, 0]
+
+// let number = parseInt(prompt("Ingrese el numero: "));
+// function matrizNumbers(number) {    
+
+        
+//         for (i=number; i>= 0; i-- ) {
+//         console.log(i);       
+//      }
+//      return [i]; }
+//  console.log(matrizNumbers(number));
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 17. Cree una función que tome una matriz y devuelva la diferencia entre los
+// números más grandes y más pequeños.
+//diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+// Smallest number is -50, biggest is 32.
+
+// let numbers = [10, 4, 1, 4, -10, -50, 32, 21];
+
+// function minMax(numbers) {
+     
+//     let min = Math.min.apply(null, numbers);
+//     let max = Math.max.apply(null, numbers); 
+//     let result = max - min;
+//     return result;
+// }
+
+// console.log(minMax(numbers));
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 18. Cree una función que filtre las cadenas de una matriz y devuelva una 
+//nueva matriz que solo contenga enteros.
+//filterList([1, 2, 3, "x", "y", 10]) ➞ [1, 2, 3, 10]
+
+// let arr = [1, 2, 3, "x", "y", 10];
+
+// let result = arr.filter(e => typeof e !== 'string');
+
+// console.log(result);
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 19. Cree una función que tome dos argumentos (elemento, tiempos). 
+//El primer argumento (elemento) es el elemento que necesita repetirse, 
+//mientras que el segundo argumento (veces) es la cantidad de veces que se
+// debe repetir el elemento. Devuelve el resultado en una matriz.
+//repeat(13, 5) ➞ [13, 13, 13, 13, 13]
+
+// var repeatelem = function(elem, n){
+//     // returns an array with element elem repeated n times.
+//     var arr = [];
+
+//     for (var i = 0; i < n; i++) {
+//         arr = arr.concat(elem);
+//     };
+
+//     return arr;
+// };
+
+// console.log(repeatelem(13, 5))
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 20. Escriba una función, .vreplace () que extienda el prototipo
+// de cadena reemplazando todas las vocales en una cadena con una vocal especificada.
+//"apples and bananas".vreplace("u") ➞ "upplus und bununus"
+
+//https://stackoverflow.com/questions/39624581/javascript-replace-characters-of-an-element-in-an-array/39624661
+
+/*------------------------------------------------------------------------------------------------------------*/
+// 22. Cree una función que capitalice la última letra de cada palabra.
+//capLast("hello") ➞ "hellO"
+//http://js.dokry.com/cmo-capitalizar-la-ltima-letra-de-cada-palabra-en-una-cadena.html
 
 
